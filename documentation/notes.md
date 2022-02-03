3827e80ecbb67132f0137fc86c46ce3668178304
luizaugustoventu1998@hotmail.com <luiz.ventura@a2wplataforma.com.br>
2022-02-02 22:32:28 -0300

    Estrutura básica e primeiro teste

---
+++ documentation/notes.md
@@ -0,0 +1,11 @@
+# Anti-Patterns/Code Smells
+- Speculative Generality
+- God Class
+- Divergent Change
+- Blank Lines
+- Improper Instantiation (nome não-oficial)
+
+# Design Patterns/Principles/Conventions
+- You Ain't Gonna Need It (YAGNI)
+- Single Responsability (SRP)
+- Arrange, Act, Assert (AAA)
---
+++ tests/check-last-event-status.spec.ts
@@ -0,0 +1,22 @@
+class CheckLastEventStatus {
+    constructor (private readonly loadLastEventRepository: LoadLastEventRepository) {}
+
+    async perform (groupId: string): Promise<void> {
+
+    }
+}
+
+class LoadLastEventRepository {
+    groupId?: string
+}
+
+describe('CheckLastEventStatus', () => {
+    it('should get last event data', async () => {
+        const loadLastEventRepository = new LoadLastEventRepository()
+        const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository)
+
+        await checkLastEventStatus.perform('any_group_id')
+
+        expect(loadLastEventRepository.groupId).toBe('any_group_id')
+    })
+})
