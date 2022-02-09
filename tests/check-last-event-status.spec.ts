3a0aef94bc0ad1abfd053a7e60dfab719334278e
luizaugustoventu1998@hotmail.com <luiz.ventura@a2wplataforma.com.br>
2022-02-09 01:37:30 -0300

    Retornar status inReview quando now = review time

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -109,9 +109,25 @@ describe('CheckLastEventStatus', () => {
     })
 
     it('should return status inReview when now is before review time', async () => {
+        const reviewDurationInHours = 1
+        const reviewDurationInMs = reviewDurationInHours * 60 * 60 * 1000
         const { sut, loadLastEventRepository } = makeSut()
         loadLastEventRepository.output = {
-            endDate: new Date(new Date().getTime() - 1),
+            endDate: new Date(new Date().getTime() - reviewDurationInMs + 1),
+            reviewDurationInHours
+        }
+
+        const eventStatus = await sut.perform({ groupId })
+
+        expect(eventStatus.status).toBe('inReview')
+    })
+
+    it('should return status inReview when now is equal to review time', async () => {
+        const reviewDurationInHours = 1
+        const reviewDurationInMs = reviewDurationInHours * 60 * 60 * 1000
+        const { sut, loadLastEventRepository } = makeSut()
+        loadLastEventRepository.output = {
+            endDate: new Date(new Date().getTime() - reviewDurationInMs),
             reviewDurationInHours: 1
         }
 
